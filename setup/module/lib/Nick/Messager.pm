1ea864a61c47992c8ad8fa78eebebbc674f42ef4
Nick Peskett <github.perl@peskett.co.uk>
2021-09-12 07:30:08 +0100

    Explicitly set blocking and export the variable.

--- setup/module/lib/Nick/Messager.pm
+++ setup/module/lib/Nick/Messager.pm
@@ -7,12 +7,16 @@ use base qw( Nick::StandardBase Exporter );
 
 use IO::Socket::INET;
 
-our( $VERSION, @EXPORT_OK, %MESSAGER_PORTS, $MESSAGER_SERVER, $MESSAGER_TIMEOUT );
+our(
+    $VERSION, @EXPORT_OK, %MESSAGER_PORTS, $MESSAGER_SERVER, $MESSAGER_TIMEOUT,
+    $MESSAGER_BLOCKING
+);
 
 BEGIN {
     $VERSION = '1.00';
     @EXPORT_OK = qw(
         %MESSAGER_PORTS $MESSAGER_SERVER $MESSAGER_TIMEOUT
+        $MESSAGER_BLOCKING
     );
     %MESSAGER_PORTS = qw(
         producer 8024
@@ -20,6 +24,7 @@ BEGIN {
     );
     $MESSAGER_SERVER = '127.0.0.1';
     $MESSAGER_TIMEOUT = 5;
+    $MESSAGER_BLOCKING = 1;
 }
 
 sub connect {
@@ -34,7 +39,8 @@ sub connect {
             'PeerPort'  => $MESSAGER_PORTS{$type},
             'Proto'     => 'tcp',
             'Type'      => SOCK_STREAM,
-            'Timeout'   => $MESSAGER_TIMEOUT
+            'Timeout'   => $MESSAGER_TIMEOUT,
+            'Blocking'  => $MESSAGER_BLOCKING
         ) or $class -> throw(
             "Unable to connect to $type messager at $MESSAGER_SERVER:$MESSAGER_PORTS{$type}: $@"
         )
